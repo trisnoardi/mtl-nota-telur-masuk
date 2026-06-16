3ff878fb88c2419eea2b8ee6ba0e630685e53cca
Trisno Ardi AFK-0 <trisnoardi93@gmail.com>
2026-06-16 22:15:02 +0800

    fix: add cache buster headers + meta tags to force refresh

--- nota-telur-masuk.php
+++ nota-telur-masuk.php
@@ -76,12 +76,19 @@ foreach ([$paidDir, $unpaidDir] as $dir) {
     }
 }
 $initialPosJson = json_encode($initialPos);
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+$cacheBuster = time();
 ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>PO Pro - Mitra Telur Premium</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
